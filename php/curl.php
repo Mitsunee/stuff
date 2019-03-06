65b17b3bb7ca4154bc1dd1c75b71a020acc19bb7
Mitsunee <michelle@mitsunee.com>
2019-03-06 13:30:05 +0100

    Update curl.php

--- php/curl.php
+++ php/curl.php
@@ -5,6 +5,7 @@
  * This function can be used for easy curl requests. It replaces curl_exec as well as any setup before that in your code, as it simply returns whatever curl_exec did.
  *
  * @author Mitsunee <https://www.mitsunee.com>
+ * @license https://github.com/Mitsunee/stuff/blob/master/LICENSE
  *
  * @param string $url The url to grab with curl
  * @return string 
