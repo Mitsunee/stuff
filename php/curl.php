7d12ed6cf92de8710204247ddd1947f6bb07c777
Mitsunee <michelle@renzio42.com>
2019-03-06 13:27:40 +0100

    Create curl.php

---
+++ php/curl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * one-time curl request
+ *
+ * This function can be used for easy curl requests. It replaces curl_exec as well as any setup before that in your code, as it simply returns whatever curl_exec did.
+ *
+ * @author Mitsunee <https://www.mitsunee.com>
+ *
+ * @param string $url The url to grab with curl
+ * @return string 
+ */
+function curlRequest($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $ret = curl_exec($ch);
+    curl_close($ch);
+    return $ret;
+}
+?>
