9b1f10bb9d80d914ff3c4c8ab815aba9f73ce847
Mitsunee <michelle@mitsunee.com>
2019-05-13 11:14:29 +0200

    Update autoclick-mii.user.js
    now clicks faster and clears itself after 20 seconds to make the page perfectly operational again (not that I ever found any issue like that)

--- userscripts/autoclick-mii.user.js
+++ userscripts/autoclick-mii.user.js
@@ -1,14 +1,19 @@
 // ==UserScript==
 // @name         Autoclick Mii
-// @version      0.1
+// @version      0.2
 // @description  Autoclicks Mii every second to gather random platin points
 // @author       Mitsunee
 // @match        https://my.nintendo.com/
 // @grant        none
 // @icon        https://my.nintendo.com/favicon.ico
+// @run-at      document-end
 // ==/UserScript==
 
-setInterval(function() {
+// interval to click Mii every 500ms
+let miiClicker = setInterval(function() {
     document.querySelector(".MiiStage_btns>a.mii").click();
-    //Note: idk if this clicks the weekly Platin points yet, I already got mine for this week, so I can't check the class names
-}, 1000);
+}, 500);
+// timeout to clear the interval after 20 seconds
+setTimeout(function() {
+    clearInterval(miiClicker);
+}, 20000);
