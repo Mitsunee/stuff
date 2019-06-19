877dbc61ce230933d98407f7a6bfbea127e702f0
Mitsunee <michelle@mitsunee.com>
2019-06-19 22:18:33 +0200

    add support for url format of mobile twitter
    no clue why they use ?format=jpg instead of using a file extension normally like us mere mortals would

--- userscripts/twimg to orig.user.js
+++ userscripts/twimg to orig.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        twimg to orig
-// @version     2
+// @version     2.1
 // @author      Mitsunee
 // @grant       none
 // @include     https://pbs.twimg.com/media/*
@@ -8,21 +8,32 @@
 // ==/UserScript==
 const loc = location.href;
 function twimgToOrig() {
-    const i = loc.substring(8).indexOf(':'),
+    const h = loc.indexOf('format='),
+        i = loc.substring(8).indexOf(':'),
         j = loc.indexOf('?');
     let newLoc = loc;
+    // check if format=ext was used and fix it
+    if (h != -1) {
+        newLoc = loc.substring(0, h-1);
+        let fileExt = loc.match(/format=([a-zA-Z]+)/);
+        if (fileExt === null) alert("Could not convert twitter url to orig");
+        newLoc += "." + fileExt[1];//only the actual match
+        return newLoc;
+    }
     // check if : already exists and purge it
     if (i != -1) {
-        newLoc = location.href.substring(0, i+8);
+        newLoc = loc.substring(0, i+8);
+        return newLoc;
     }
     // check if ? already exists and purge it
     if(j != -1) {
-        newLoc = location.href.substring(0, j);
+        newLoc = loc.substring(0, j);
+        return newLoc;
     }
-    // redirect
-    location.href = newLoc + "?name=orig";
 }
 
 if (!loc.endsWith("?name=orig")) {
-    twimgToOrig();
+    let res = twimgToOrig();
+    // redirect
+    location.href = res + "?name=orig";
 }
