211ac71208bb81001f78ac3fbcfd0dc05fb1da9b
Mitsunee <michelle@mitsunee.com>
2019-08-04 12:37:19 +0200

    update pixiv bookmarklet and improve instructions for all

--- bookmarklets/find pixiv post.js
+++ bookmarklets/find pixiv post.js
@@ -1,5 +1,5 @@
 /*
- * Copy the entire line below, then add it as the url to a new bookmark.
- * Works with every url that has a pixiv-formatted name like "30874165_p0.jpg" in it.
+ * Copy the entire line below, then add it as the url to a new bookmark (rightclick into bookmarks bar and select paste)
+ * Works with every url that has a pixiv-formatted name like "30874165_p0.jpg" or "30874165_p0_master1200.jpg" in it.
  */
-javascript:(()=>{m=location.href.match(/\/([0-9]+)_p[0-9]+\./);if(m!=null) location.href="https://www.pixiv.net/member_illust.php?mode=medium&illust_id="+m[1];})();
+javascript:(() => {m = location.href.match(/\/([0-9]+)_p[0-9]+[\._]{1}/);if (m != null) location.href = "https://www.pixiv.net/member_illust.php?mode=medium&illust_id=" + m[1];})();
