3f951cccef227cd6b9eb53234be165954deb589a
Mitsunee <michelle@mitsunee.com>
2019-11-29 18:33:50 +0100

    update spotify script to keep resId again

--- ubuntu-bash/nowPlayingSpotify.sh
+++ ubuntu-bash/nowPlayingSpotify.sh
@@ -2,17 +2,31 @@
 
 # Quick bash script that allows you to grab the currently playing song from Spotify
 # Based on: https://obsproject.com/forum/resources/now-playing-bash-script.341/
-# This program uses sed make sure it's installed
 
 # Config vars
 updateInterval=2 # In seconds
 outputFile="./nowPlaying.txt"
 
 # Functions
+<<<<<<< HEAD
+function grabResID() {
+    window="$(xwininfo -tree -root | grep '"Spotify' | egrep --invert-match 'spotify\": \(\"spotify')"
+    cutSpaces="${window##*0x}"
+    resID="0x${cutSpaces%% \"*}"
+}
+
+function grabXOutput() {
+	xOutput="$(xwininfo -tree -root | grep $resID)"
+}
+
+grabResID
+echo "Spotify window resID: $resID"
+=======
 function grabXOutput() {
 	xOutput="$(xwininfo -tree -root | grep \"Spotify | egrep --invert-match 'spotify\": \(\"spotify')" # Get all open windows and output only the one from spotify not titled "spotify"
 }
 
+>>>>>>> 5602aa0be982f1b51603cc24cf38d11ae7ee4c42
 echo "Press CTRL+C to exit the script"
 printf "\n"
 
