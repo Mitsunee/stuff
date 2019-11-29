b7de881e2eaad114ab1a019728043e05f8c3c77d
Mitsunee <michelle@mitsunee.com>
2019-11-29 11:26:05 +0100

    Update nowPlayingSpotify.sh
    now finds the window itself

--- ubuntu-bash/nowPlayingSpotify.sh
+++ ubuntu-bash/nowPlayingSpotify.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Quick bash script that allows you to grab the currently playing song from Spotify
-# Based on: https://obsproject.com/forum/resources/now-playing-bash-script.341/
 # This program uses sed make sure it's installed
 
 # Config vars
@@ -9,20 +8,10 @@ updateInterval=2 # In seconds
 outputFile="./nowPlaying.txt"
 
 # Functions
-xwinlength=21 # The length of this string "xwininfo: Window id: "
-function grabResourceID() {
-	xwinoutput="$(xwininfo | grep 'Window id:')" # Grab the line containing the resource id from xwininfo
-	cutoutput="${xwinoutput:xwinlength}" # Format it
-	resourceID="${cutoutput%% *}"
-}
-
 function grabXOutput() {
-	xOutput="$(xwininfo -tree -root | grep $resourceID)" # Get all open windows and output only the one with the resource id of the chosen player
+	xOutput="$(xwininfo -tree -root | grep \"Spotify | egrep --invert-match 'spotify\": \(\"spotify')" # Get all open windows and output only the one from spotify not titled "spotify"
 }
 
-echo "Click in the Spotify Client window..."
-grabResourceID
-echo "Resource ID is $resourceID"
 echo "Press CTRL+C to exit the script"
 printf "\n"
 
@@ -38,11 +27,6 @@ while true; do
 	oldOutput=$xOutput
 	cutColon="${xOutput%%\":*}"
 	songName="${cutColon##* \"}"
-
-	# Testing
-	#echo "xOutput $xOutput"
-	#echo "cutColon $cutColon"
-	#printf "\n"
 
 	# Output to console and file, unless playback is paused
 	if [ "$songName" != "Spotify Premium" ]; then # Playback not paused
