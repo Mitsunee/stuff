a3ce56d2fa929f5e0b418217a15f294d9b2e3c6d
Mitsunee <michelle@mitsunee.com>
2020-11-18 18:20:35 +0100

    Update range.js

--- javascript/range.js
+++ javascript/range.js
@@ -1,7 +1,9 @@
 /*
  * range
  *
- * generates an array with all integers from min to max
+ * generates an array with all integers between and including two integers (or one integer and 0)
+ * (see below for minimized version!)
+ *
  * int min - minimum. If no maximum is giving this argument will be used as maximum instead and minimum will be assumed to be 0
  * int max (optional, see above) - maximum.
  *
@@ -20,3 +22,8 @@ function range(min, max) {
     let n = max - min + 1;
     return Array(n).fill(0).map((_,index)=>(index+min));
 }
+
+/*
+ * range (minimized)
+ */
+function range(a,b){if(isNaN(b)&&(b=a,a=0),a>b){let t=a;a=b,b=t}let n=b-a+1;return Array(n).fill(0).map((_,i)=>(i+a));}
