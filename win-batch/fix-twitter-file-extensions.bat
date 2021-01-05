d33901c434194358c58648cc073af5ce0eb45587
Mitsunee <michelle@mitsunee.com>
2021-01-05 19:45:23 +0100

    cleanup

--- win-batch/fix-twitter-file-extensions.bat
+++
@@ -1,4 +0,0 @@
-ren "*.jpg_large" "*.jpg"
-ren "*.png_large" "*.png"
-ren "*.jpg_orig" "*.jpg"
-ren "*.png_orig" "*.png"
--- win-batch/optipng-all.bat
+++
@@ -1,5 +0,0 @@
-REM You need to either have optipng in the same folder as this script or add it to %PATH%
-echo This script will run optipng on all PNG files in its folder
-pause
-optipng.exe *.png
-pause
