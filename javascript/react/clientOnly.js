7485f82c61dd6b52efe10e5a7753f7f243ee0c12
Mitsunee <michelle@mitsunee.com>
2021-07-17 23:34:27 +0200

    Update clientOnly.js

--- javascript/react/clientOnly.js
+++ javascript/react/clientOnly.js
@@ -2,10 +2,12 @@
 /// if you are using some other framework with SSR adjust the imports if required
 import { useState, useEffect, useLayoutEffect } from "react";
 
+// use this hook if you want to do specific computation (like randomization) only in the client
 export const useClientOnlyEffect = typeof window !== 'undefined' 
-  ? useLayoutEffect 
+  ? useLayoutEffect // layout effects are not rendered in serverside rendering
   : useEffect;
 
+// use this hook if you want a boolean to use in ternary operators
 export function useIsClient() {
   const [isClient, setIsClient] = useState(false);
   
@@ -16,6 +18,7 @@ export function useIsClient() {
   return isClient;
 }
 
+// or use this component
 export function NoSSR({ children }) {
   const isClient = useIsClient();
   
