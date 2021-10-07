318d28dcad00fd79368664b5c12b1b6774d8b2f1
Mitsunee <michelle@mitsunee.com>
2021-10-07 21:02:54 +0200

    Delete youtube-temp old design.js

--- bookmarklets/youtube-temp old design.js
+++
@@ -1,5 +0,0 @@
-/*
- * Copy the entire line below, then add it as the url to a new bookmark (rightclick into bookmarks bar and select paste)
- * This bookmarklet adds the variable to disable polymer (the new design) to the url to temporarily switch to the old design.
- */
- javascript:(()=>{location.href+=(location.href.indexOf("?")!=-1?"&":"?")+"disable_polymer=true";})();
