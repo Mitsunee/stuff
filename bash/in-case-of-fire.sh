f24e9c11e27ac81ac32395388c607409b5292b25
Mitsunee <michelle@mitsunee.com>
2021-11-21 19:50:37 +0100

    Update in-case-of-fire.sh

--- bash/in-case-of-fire.sh
+++ bash/in-case-of-fire.sh
@@ -5,5 +5,9 @@
 # all with just a single action!
 #
 # It can be included in .bashrc or any other file imported by your shell's rc file
+#
+# Features suggestions by @Ikaheishi (https://github.com/Ikaheishi)
+# - `git branch --show-current` to include current working branch in pushed branch
+# - `git add -A` so the command works correctly in any directory in the workspace instead of only the workspace's root dir
 
 alias git-fire="TEMP=FIRE-\$USER-\$(git branch --show-current);git add -A && git commit -m 'oh shit a fire'; git branch \"\$TEMP\" && git push -u origin \"\$TEMP\" && shutdown now"
