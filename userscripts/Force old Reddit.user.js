8d168bf9eda550080817323f34613deebc21f12c
Mitsunee <michelle@mitsunee.com>
2022-07-28 08:59:05 +0200

    Update Force old Reddit.user.js
    - Now doesn't break gallery urls
    - Ignores frontpage of r/place, so hopefully it'll work next time
    - added description to point out that new.reddit.com is not matched

--- userscripts/Force old Reddit.user.js
+++ userscripts/Force old Reddit.user.js
@@ -1,9 +1,14 @@
 // ==UserScript==
 // @name        Force old Reddit
-// @version     1
+// @description Forces old Reddit if possible, does not apply to new.reddit.com
+// @version     2
 // @grant       none
-// @include     https://www.reddit.com/*
-// @include     http://www.reddit.com/*
+// @match     https://www.reddit.com/*
+// @match     http://www.reddit.com/*
 // @icon        https://www.google.com/s2/favicons?domain=www.reddit.com
 // ==/UserScript==
-location.href = "https://old" + location.href.slice(location.href.indexOf(".reddit.com"));
+(() => {
+    if (location.href.match(/\.com\/gallery/) !== null) return; // galleries are not supported on old reddit
+    if (location.href.match(/\.com\/r\/place\/?$/) !== null) return; // r/place didn't work on old reddit last time
+    location.href = "https://old" + location.href.slice(location.href.indexOf(".reddit.com"));
+})()
