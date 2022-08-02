bc7e8a30949ee363e9a321ba72c1ae410db38040
Mitsunee <michelle@mitsunee.com>
2022-08-02 11:20:04 +0200

    Update gamepress-guide-next-step.js

--- bookmarklets/gamepress-guide-next-step.js
+++ bookmarklets/gamepress-guide-next-step.js
@@ -2,4 +2,4 @@
  * Copy the entire line below, then add it as the url to a new bookmark (rightclick into bookmarks bar and select paste)
  * This bookmarklet will automatically scroll the next step in a gamepress.gg/grandorder guide into view.
  */
-javascript:(()=>document.querySelector("input[type='checkbox']:not(:checked)").scrollIntoViewIfNeeded())();
+javascript:(document.querySelector(".walk-checkbox:not(:checked)").parentElement.parentElement.scrollIntoView())()
