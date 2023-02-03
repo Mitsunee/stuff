16195f2106282a774b582efdace6bc2a3953d239
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-02-03 10:52:13 +0100

    fix mastodon follow bookmarklet

--- bookmarklets/mastodon-on-my-instance.js
+++ bookmarklets/mastodon-on-my-instance.js
@@ -1,7 +1,7 @@
 /**
  * Copy the entire line below, then add it as the url to a new bookmark (rightclick into bookmarks bar and select paste)
  * 
- * This bookmarklet attempts to redirect a Mastodon profile to a customizable target instance
+ * This bookmarklet attempts to redirect a Mastodon profile to the follow screen on your mastodon instance
  * 
  * Simply edit the bookmark created when pasting, probably change the title and replace "ieji.de" with the domain
  * name of the instance you are on, then click the bookmark whenever you are on a profile on a different instance
@@ -10,16 +10,15 @@
 
 /* Copy this:
 
-javascript:(()=>{let t="ieji.de",o=new URL(window.location),i=o.pathname.match(/^(\/@[^\/@]*)/);i&&o.host!=t&&(window.location="https://"+t+i[1]+"@"+o.host)})()
+javascript:(()=>{let t=new URL(window.location),i=t.pathname.match(/^\/(@[^\/@]*)/);i&&(window.location="https://ieji.de/authorize_interaction?uri="+i[1]+"@"+t.host)})();
 
 */
 
 // readable version:
 
 (() => {
-  let targetInstance = "ieji.de",
-    url = new URL(window.location),
-    match = url.pathname.match(/^(\/@[^\/@]*)/);
-  if (!match || url.host == targetInstance) return;
-  window.location = "https://" + targetInstance + match[1] + "@" + url.host;
+  let url = new URL(window.location),
+    match = url.pathname.match(/^\/(@[^\/@]*)/);
+  if (!match) return;
+  window.location = "https://ieji.de/authorize_interaction?uri=" + match[1] + "@" + url.host;
 })();
