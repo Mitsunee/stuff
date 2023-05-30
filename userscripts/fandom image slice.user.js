482132005e8387d779dee6b9de86c420bbad0bf6
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-05-30 09:37:12 +0200

    refactor fandom image slice userscript and add webp support

--- userscripts/fandom image slice.user.js
+++ userscripts/fandom image slice.user.js
@@ -1,12 +1,19 @@
 // ==UserScript==
 // @name        fandom image slice
-// @version     1.1
+// @description Cleans up url of fandom images so they are shown at original size and more convenient to save
+// @version     1.2
 // @author      Mitsunee
 // @grant       none
 // @run-at      document-end
-// @include     https://static.wikia.nocookie.net/*.png/*
-// @include     https://static.wikia.nocookie.net/*.jpg/*
+// @match       https://static.wikia.nocookie.net/*.png/*
+// @match       https://static.wikia.nocookie.net/*.jpg/*
+// @match       https://static.wikia.nocookie.net/*.webp/*
 // @icon        https://static.wikia.nocookie.net/qube-assets/f2/3267/favicons/favicon.ico
 // ==/UserScript==
-if(location.href.indexOf(".png")!=-1) location.href = location.href.slice(0,location.href.indexOf(".png")+4);
-if(location.href.indexOf(".jpg")!=-1) location.href = location.href.slice(0,location.href.indexOf(".jpg")+4);
+
+(() => {
+  let i;
+  if((i = location.href.indexOf(".png")) != -1) location.href = location.href.slice(0,i + 4);
+  if((i = location.href.indexOf(".jpg")) != -1) location.href = location.href.slice(0,i + 4);
+  if((i = location.href.indexOf(".webp")) != -1) location.href = location.href.slice(0,i + 5);
+})()
