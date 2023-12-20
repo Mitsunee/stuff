99669e03b42c22affa5742ee640c83dfc2c55371
Mitsunee <19474909+Mitsunee@users.noreply.github.com>
2023-12-20 22:15:21 +0100

    add: handle recipes outputting more than one of the output item in craft-helper

--- python/minecraft-craft-helper/README.md
+++ python/minecraft-craft-helper/README.md
@@ -1,6 +1,6 @@
 # Minecraft Craft Helper
 
-Recursively resolves provided crafting recipes to totals of components
+Recursively resolves provided crafting recipes to totals of components and provides detailed crafting instructions
 
 ## Installation
 
@@ -12,8 +12,6 @@ Minecraft Craft Helper uses the following packages which you may need to install
 pip install schema argparse pyyaml queue
 ```
 
-The LibYaml integration of PyYaml is supported and will be used if available
-
 ## Usage
 
 ```
@@ -22,24 +20,49 @@ $ python craft-helper.py [-h] --recipes <file> items [items ...]
 
 ## Recipes File
 
-Place all recipes that should be resolved to their components into a yaml file. An [example file](./recipes-example.yml) and [schema](./recipes-schema.json) have been provided with the script. The file should contain a list of item objects containing a name and a list of ingredients, where each ingredient must also have a name and may optionally have an amount (the default amount is 1, see *Redstone Torch* in the example file).
+Place all recipes that should be resolved to their components into a yaml file. An [example file](./recipes-example.yml) and [schema](./recipes-schema.json) have been provided with the script. The file should contain a list of item objects containing a `name`, an optional amount of `outputs` (read: amount of the item created by the crafting recipe, default: 1) and a list of `ingredients`, where each ingredient must also have a `name` and may optionally have an `amount` (default: 1).
+
+```yml
+- name: Redstone Repeater
+  # outputs is assumed to be 1
+  ingredients:
+    - name: Redstone Torch
+      amount: 2
+    - name: Redstone
+      # amount is assumed to be 1
+    - name: Stone
+      amount: 3
+- name: Wood Plank
+  outputs: 4
+  ingredients:
+    - name: Wood Logs
+```
 
 ## Example
 
 ```
-$ python craft-helper.py -r recipes-example.yml "Redstone Torch,3" "Redstone Block"
+$ python craft-helper.py -r recipes-example.yml "Redstone Repeater,2" "Redstone Block"
 ```
 
 **Output:**
 
 ```
 Crafting List:
-  - Redstone Torch: 3
+  - Redstone Repeater: 2
   - Redstone Block: 1
 
 Total Ingredients:
-  - Stick: 3
-  - Redstone: 12
+  - Redstone: 15
+  - Cobblestone: 6
+  - Wood Logs: 1
+
+Steps:
+  - Craft 4 of Wood Plank
+  - Craft 1 of Redstone Block
+  - Craft 6 of Stone
+  - Craft 4 of Stick
+  - Craft 4 of Redstone Torch
+  - Craft 2 of Redstone Repeater
 ```
 
 ## License
--- python/minecraft-craft-helper/craft-helper.py
+++ python/minecraft-craft-helper/craft-helper.py
@@ -1,7 +1,7 @@
 """
 Minecraft Crafting Helper
 
-Recursively resolves provided crafting recipes to totals of components
+Recursively resolves provided crafting recipes to totals of components and provides detailed crafting instructions
 
 usage: craft-helper.py [-h] -r <file> items [items ...]
 
@@ -35,34 +35,37 @@ import argparse
 import re
 import yaml
 from queue import Queue
-try:
-  from yaml import CLoader as Loader
-except ImportError:
-  from yaml import Loader
+import math
 
 class ItemWithAmount:
-  """Class that accepts single input of name followed by an optional amount"""
-  def __init__(self, item, amount):
-    self.item = item
+  """Class that accepts an item name and an amount"""
+  def __init__(self, name, amount):
+    self.name = name
     self.amount = amount
 
+# compat function for argparse to use ItemWithAmount
 def item_argument(input_str):
   p = re.compile('([a-zA-Z ]+)( *(,|;|\*) *(\\d+))?')
   res = p.match(input_str)
-  item = res.group(1)
+  name = res.group(1)
   amount = 1
   try:
     amount = int(res.group(4))
   except:
     pass
-  return ItemWithAmount(item=item, amount=amount)
+  return ItemWithAmount(name=name, amount=amount)
 
+# input validation for recipes file
 def validate_recipes(recipes):
   recipes_dict = {}
   schema = Schema({
     'name': Use(str, error='Item name must be string'),
+    Optional('outputs', default=1): int,
     'ingredients': And(
-      [{'name': Use(str, error='Ingredient name must be string'), Optional('amount', default=1): int}],
+      [{
+        'name': Use(str, error='Ingredient name must be string'),
+        Optional('amount', default=1): int
+      }],
       lambda l: len(l) >= 1
     )
   })
@@ -82,11 +85,13 @@ def validate_recipes(recipes):
 
   return recipes_dict
 
-# Program config
+# Program config and argument parsing
 parser = argparse.ArgumentParser(
   prog='craft-helper',
-  description='Recursively resolves provided crafting recipes to totals of components',
-  epilog='This script is licensed under the WTFPL license (see http://www.wtfpl.net/)'
+  description="""Recursively resolves provided crafting recipes to totals of
+  components and provides detailed crafting instructions""",
+  epilog="""This script is licensed under the WTFPL license(see
+  http://www.wtfpl.net/)"""
 )
 parser.add_argument(
   '-r', '--recipes',
@@ -104,70 +109,116 @@ parser.add_argument(
   quotes, separating the two values with either comma (,), semicolon (;), or
   asterisk (*). You may put spaces before or after the separator as you wish"""
 )
-
-# Parse args and create queue and dicts
 args = parser.parse_args()
-queue = Queue()
-ingredients = {}
-crafted = {}
 
 # Load recipes
 try:
   recipes_stream = open(args.recipes, mode='r')
-  recipes = validate_recipes(yaml.load(recipes_stream, Loader=Loader))
-except:
+  recipes = validate_recipes(yaml.safe_load(recipes_stream))
+except Exception as e:
   print(f"Could not read recipes file {args.recipes}")
+  print(e)
   exit(1)
+
+# create vars needed below :)
+ingredients = {} # maps item name to amount needed for raw materials (no recipe)
+inventory = {} # temporary inventory to handle recipes that output more than single item
+crafted = {} # maps item name to amount crafted
+crafted_done = set() # set of items that have been crafted
+queue = Queue() # queue of items that need to be processed
 
 # Add items from arguments to queue
 print('Crafting List:')
 for arg in args.items:
   queue.put(arg)
-  print(f"  - {arg.item}: {arg.amount}")
+  print(f"  - {arg.name}: {arg.amount}")
 
 # work through queue
 while not queue.empty():
   item = queue.get()
-  if item.item in recipes:
-    # recipe exists for item, add to crafted dict
-    if not item.item in crafted:
-      crafted[item.item] = 0
-    crafted[item.item] += item.amount
-    # queue ingredients
-    for ingredient in recipes[item.item]['ingredients']:
-      total_amount = ingredient['amount'] * item.amount
-      temp = ItemWithAmount(item=ingredient['name'],amount=total_amount)
+
+  # found an item that's a raw material
+  if not item.name in recipes:
+    if not item.name in ingredients:
+      ingredients[item.name] = 0
+    ingredients[item.name] += item.amount
+    continue
+
+  # item has a recipe, process each ingredient
+  # create crafted dict key if not existing
+  if not item.name in crafted:
+    crafted[item.name] = 0
+  crafted[item.name] += item.amount
+
+  # queue ingredients
+  recipe = recipes[item.name]
+  for ingredient in recipe['ingredients']:
+    total_amount = ingredient['amount'] * item.amount
+
+    # recipe outputs single item, simply queue for total_amount
+    if recipe['outputs'] < 2:
+      temp = ItemWithAmount(name=ingredient['name'], amount=total_amount)
       queue.put(temp)
-  else:
-    # raw material, add to ingredients dict
-    if not item.item in ingredients:
-      ingredients[item.item] = 0
-    ingredients[item.item] += item.amount
+      continue
+
+    # create inventory key if not existing
+    if not ingredient['name'] in inventory:
+      inventory[ingredient['name']] = 0
+
+    # if we already have enough of the item in inventory simply take them
+    if inventory[ingredient['name']] >= total_amount:
+      inventory[ingredient['name']] -= total_amount
+      continue
+
+    # remove from total_amount what we already had from before
+    total_amount -= inventory[ingredient['name']] # should still leave > 0 items
+    inventory[ingredient['name']] = 0
+
+    # craft more
+    crafting_ops = math.ceil(total_amount / recipe['outputs'])
+    crafted_amount = crafting_ops * recipe['outputs']
+    inventory[ingredient['name']] = crafted_amount - total_amount
+
+    # queue for crafted amount
+    temp = ItemWithAmount(name=ingredient['name'], amount=crafting_ops)
+    queue.put(temp)
 
 # output result
 print('\nTotal Ingredients:')
 for item in ingredients.keys():
   print(f"  - {item}: {ingredients[item]}")
 
-# instructions
-crafted_done = set()
+# print crafting instructions
 print('\nSteps:')
 while len(crafted_done) < len(crafted):
   # for every item that has not been crafted yet
   for item in crafted_done.symmetric_difference(crafted):
-    # check that all ingredients are raw material or have been crafted already
     ready=True
-    for ingredient in recipes[item]['ingredients']:
+    recipe = recipes[item]
+
+    # check that all ingredients are raw material or have been crafted already
+    for ingredient in recipe['ingredients']:
       # skip if it's not a craftable item
       if not ingredient['name'] in recipes:
         continue
+
       # set ready to false if it's not crafted yet and break
       if not ingredient['name'] in crafted_done:
         ready=False
         break
+
     # skip if it's not ready to be crafted
     if not ready:
       continue
+
     # print crafting instruction and add item to set
-    print(f"  - Craft {crafted[item]} of {item}")
+    crafted_amount = crafted[item]
+
+    # round up for recipes that output more than single item
+    if recipe['outputs'] > 1:
+      crafting_ops = math.ceil(crafted[item] / recipe['outputs'])
+      crafted_amount = crafting_ops * recipe['outputs']
+
+    print(f"  - Craft {crafted_amount} of {item}")
     crafted_done.add(item)
+
